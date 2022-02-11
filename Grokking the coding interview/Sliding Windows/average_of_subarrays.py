b9cbf68568c337aedcbff5ae1f7f01e7563946d3
rkabita <36805923+rkabita@users.noreply.github.com>
2022-02-11 03:17:45 -0500

    Update average_of_subarrays.py

--- Grokking the coding interview/Sliding Windows/average_of_subarrays.py
+++ Grokking the coding interview/Sliding Windows/average_of_subarrays.py
@@ -1,3 +1,12 @@
+###### Question ########
+# Given an array, find the average of all subarrays of ‘K’ contiguous elements in it.
+# Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+# Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+
+
+
+
+
 def find_averages_of_subarrays(K, arr):
   result = []
   windowSum, windowStart = 0.0, 0
